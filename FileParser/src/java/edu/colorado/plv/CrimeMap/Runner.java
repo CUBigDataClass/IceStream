a9df60b05418069beaad959627c193c667ffe650
Pezhin <p@oath.pl>
2017-03-12 00:55:16 -0700

    delete comments that are not needed

--- FileParser/src/java/edu/colorado/plv/CrimeMap/Runner.java
+++ FileParser/src/java/edu/colorado/plv/CrimeMap/Runner.java
@@ -20,49 +20,21 @@ public class Runner {
         ArrayList<Crime> crimes;
         crimes = laParser.parseToCrimeArray();
         int count = 0;
-        for(int i = 0; i < crimes.size(); i++){
-            if(crimes.get(i).mCrimeType.equals("\"BURGLARY\"")){
-                System.out.println(crimes.get(i).mCrimeType);
-                
-                ++count;
-
-            };
-        }
-        System.out.println(count);
         
         
         //connect mongoDB
-        try{     
-        	/*MongoCredential credential = MongoCredential.createCredential("icestream", "icestream", "cuicestream123");
-        	MongoClient mongoClient = new MongoClient(new ServerAddress(), Arrays.asList(credential));*/
-        	
-        	
-/*
-            MongoClient mongoClient = new MongoClient("ds127300.mlab.com" , 27300);  
-            System.out.println("Database Connection Successful!");  
-            DB db = mongoClient.getDB( "icestream" ); */
-            // insert data
-            
-            /*MongoClient mongoClient = new MongoClient(new ServerAddress(), 
-                    Arrays.asList(MongoCredential.createCredential("icestream", "icestream", "cuicestream123".toCharArray())));
-            
-        	ServerAddress server = new ServerAddress("ds127300.mlab.com" , 27300);
-        	MongoCredential credentials = MongoCredential.createCredential("icestream", "icestream", "cuicestream123".toCharArray())));
-        	MongoClient mongoClient = new MongoClient(server, credentials);*/
-        	
+        try {      
+
         	MongoClientURI uri = new MongoClientURI("mongodb://icestream:cuicestream123@ds127300.mlab.com:27300/icestream");
         	MongoClient mongoClient = new MongoClient(uri);
-        	/*MongoDatabase db = mongoClient.getDatabase("yourdatabasename");
-        	MongoCollection<Document> collection = db.getCollection("yourcollection");*/
-        	
+
             System.out.println("Database Connection Successful!");  
             DB db = mongoClient.getDB( "icestream" ); 
             
             DBCollection collection = db.getCollection("crimeCollection");
         	
             
-            //sorting data and insert to mongoDB
-            for (int i=0; i<crimes.size(); i++){
+            for (int i = 0; i < crimes.size(); i++) {
 	            HashMap<String, Object> documentMap = new HashMap<String, Object>();
 	            Crime crime = crimes.get(i); 
 	            documentMap.put("Latitude", crime.mLatitude);
@@ -75,28 +47,13 @@ public class Runner {
 	            collection.insert(new BasicDBObject(documentMap));
              
             }
+
             System.out.println("Insert finished");
-            /*
-            for (int i=0; i<crimes.size(); i++){
-            	Crime crime = crimes.get(i); 
-            	String json = "{ 'Latitude' : "+ crime.mLatitude +","+
-                    "'Longtitude' : "+ crime.mLongtitude  +","+
-                    "'Date' : "+ crime.mDate  +","+
-                    "'CrimeType' : "+ crime.mCrimeType  +","+
-                    "'Status' : "+ crime.mStatus  +","+
-                    "'Location' : "+ crime.mLocation  +"}";
-          
-            	DBObject dbObject = (DBObject)JSON.parse(json);
-            	collection.insert(dbObject);
-            }*/
-            //
             
-           
-            }catch(Exception e){  
-             System.err.println( e.getClass().getName() + ": " + e.getMessage() );  
-          }  
+        } catch (Exception e){  
+           e.printStackTrace();
+        }  
          
-        
         
         
     }

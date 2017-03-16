64480f94ea0dc50c6409b40089ee809562bf592b
YanLi26 <yali2241@colorado.edu>
2017-03-15 18:17:27 -0600

    Merge remote-tracking branch 'origin/vue' into vue
    # Conflicts:
    #	Site/src/components/Sidebar.vue

--- Site/src/components/Sidebar.vue
+++ Site/src/components/Sidebar.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
 <v-app top-toolbar left-fixed-sidebar sidebar-under-toolbar>
     <header>
         <v-toolbar class="green darken-1">
@@ -30,6 +31,35 @@
             </v-list>-->
         </v-sidebar>
     <v-content class="pt-0">
+=======
+    <v-app top-toolbar left-fixed-sidebar sidebar-under-toolbar>
+        <header>
+            <v-toolbar>
+                <v-toolbar-side-icon @click.native.stop="openSidebar()"></v-toolbar-side-icon>
+                <v-toolbar-logo class="px-3">
+                    Green Arrow
+                </v-toolbar-logo>
+            </v-toolbar>
+        </header>
+        <main>
+            <v-sidebar fixed class="white--text" v-model="sidebar">
+                <div class="px-3">
+                    <h3 class="white--text mt-3">Sidebar</h3>
+                    <p>This is just an example sidebar.</p>
+                </div>
+                <v-list>
+                    <v-list-tile v-for="item in items" :key="item.title" :href="item.href" :router="item.router" avatar>
+                        <v-list-tile-avatar>
+                            <v-icon>{{item.icon}}</v-icon>
+                        </v-list-tile-avatar>
+                        <v-list-tile-content>
+                            {{ item.title }}
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </v-list>
+            </v-sidebar>
+            <v-content class="pt-0">
+>>>>>>> origin/vue
                 <v-container fluid>
                      <transition mode="out-in">
                          <router-view></router-view>

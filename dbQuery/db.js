fc0e42ed250298ef0eb631f9f761d166317fce60
ChihWei <cwl.chihweil@gmail.com>
2017-03-19 17:50:32 -0600

    Add filters

--- dbQuery/db.js
+++ dbQuery/db.js
@@ -17,16 +17,10 @@ function queryDB(num){
 	var query = { Latitude: 1, Longtitude: 1 }
 	var positionList = []
 	mycollection.find({}, query).limit(num).skip(0, function (err, docs) { 
-		//console.log(docs)
-		var position
-		for(var index in docs){ 
-			var position = [ docs[index]["Latitude"], docs[index]["Longtitude"] ]
-			//console.log(position)
-			positionList.push(position)
-		}
-		console.log(positionList)
-
+		console.log(docs)
 		db.close()
+		return docs
+		
 	})
 
 	return positionList

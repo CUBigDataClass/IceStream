05b4e7d8e0f7fc6f8b6dbb8e3770d6414ec27bae
Pezhin <p@oath.pl>
2017-03-21 22:34:41 -0600

    took a lot efford to figure this out, don't want to miss anything, so just commit

--- Site/src/components/Hello.vue
+++ Site/src/components/Hello.vue
@@ -1,20 +1,37 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <router-link to="/login">Go to Login</router-link>
-    </ul>
+    <div id="map" class="map"></div>
+    <ol-map>
+    </ol-map>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import ol from '../olwrapper/main'
+Vue.use(ol)
 export default {
   name: 'hello',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  created: function () {
+    console.log(ol)
+    ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([37.41, 8.82]),
+        zoom: 4
+      })
+    })
+    console.log('sucess')
   }
 }
 </script>
